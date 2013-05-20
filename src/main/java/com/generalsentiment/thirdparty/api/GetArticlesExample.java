f0f557dc2789c87a1f5a3aa086da112bb947e593
jdevjram <jayadev.jayaraman@generalsentiment.com>
2013-05-20 07:20:39 -0400

    Fixing examples.

--- src/main/java/com/generalsentiment/thirdparty/api/GetArticlesExample.java
+++ src/main/java/com/generalsentiment/thirdparty/api/GetArticlesExample.java
@@ -1,8 +1,7 @@
 /*
- * To try this example, download gsapi-scribe-examples-1.0.0.tgz
- * from <**-insert-link-here-**> .
+ * To try this example, get the repo at https://github.com/jdevjram/gsapi-scribe-examples
  *
- * Extract the tarball and run "mvn clean package" in the extracted folder.
+ * Run "mvn clean package" on the downloaded code.
  * This will assemble a jar-file with the examples and the scribe dependency.
  *
  * To run the example :
